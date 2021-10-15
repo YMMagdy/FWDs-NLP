874a60fcf6dbc3f8ad56486564d69237d84800ba
Youssef Magdy <52387026+YMMagdy@users.noreply.github.com>
2021-10-15 15:08:11 +0200

    Add files via upload

---
+++ dist/precache-manifest.8192d39e920e842dd2872acc7a4af36a.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49ead5325c010f09c1da8012b757535c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "34f00b3e64d72d3cb906",
+    "url": "main.css"
+  },
+  {
+    "revision": "34f00b3e64d72d3cb906",
+    "url": "main.js"
+  }
+]);
